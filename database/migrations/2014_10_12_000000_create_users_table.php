c2d0470d5e860d89484bf2065b31e9c72f06b9ac
KeoChamraeun <keochamraeun54@gmail.com>
2025-04-28 10:39:32 +0700

    ddd

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email', 191)->unique(); // 191 × 4 = 764 bytes (< 1000)            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->boolean('is_active');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
