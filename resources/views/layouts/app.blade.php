bc310f9ca2ddaeff537361b4f843a339399fd40e
KeoChamraeun <keochamraeun54@gmail.com>
2025-05-15 22:42:49 +0700

    dd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
         <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Roboto:wght@400;500&display=swap"
             rel="stylesheet">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet"> <!-- Laravel correct -->
 
         <style>
             @font-face {
