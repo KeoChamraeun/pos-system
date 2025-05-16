40354bcaf887b58ffebd47b8731ed6f53135a579
KeoChamraeun <keochamraeun54@gmail.com>
2025-05-16 09:22:20 +0700

    dd

--- Modules/Currency/Resources/views/partials/actions.blade.php
+++ Modules/Currency/Resources/views/partials/actions.blade.php
@@ -1,12 +1,19 @@
-<a href="{{ route('product-categories.edit', $product->id) }}" class="btn btn-info btn-sm">
-    <i class="bi bi-pencil"></i>
-</a>
-
-<form id="destroy{{ $product->id }}" class="d-inline" action="{{ route('product-categories.destroy', $product->id) }}"
-    method="POST" onsubmit="return confirm('Are you sure? It will delete the data permanently!')">
-    @csrf
-    @method('delete')
-    <button type="submit" class="btn btn-danger btn-sm">
+@can('edit_currencies')
+    <a href="{{ route('currencies.edit', $data->id) }}" class="btn btn-info btn-sm">
+        <i class="bi bi-pencil"></i>
+    </a>
+@endcan
+@can('delete_currencies')
+    <button id="delete" class="btn btn-danger btn-sm" onclick="
+        event.preventDefault();
+        if (confirm('Are you sure? It will delete the data permanently!')) {
+        document.getElementById('destroy{{ $data->id }}').submit()
+        }
+        ">
         <i class="bi bi-trash"></i>
+        <form id="destroy{{ $data->id }}" class="d-none" action="{{ route('currencies.destroy', $data->id) }}" method="POST">
+            @csrf
+            @method('delete')
+        </form>
     </button>
-</form>
+@endcan
--- Modules/Product/Resources/views/categories/partials/actions.blade.php
+++ Modules/Product/Resources/views/categories/partials/actions.blade.php
@@ -1,15 +1,12 @@
-<a href="{{ route('product-categories.edit', $data->id) }}" class="btn btn-info btn-sm">
+<a href="{{ route('product-categories.edit', $product->id) }}" class="btn btn-info btn-sm">
     <i class="bi bi-pencil"></i>
 </a>
-<button id="delete" class="btn btn-danger btn-sm" onclick="
-    event.preventDefault();
-    if (confirm('Are you sure? It will delete the data permanently!')) {
-        document.getElementById('destroy{{ $data->id }}').submit();
-    }
-    ">
-    <i class="bi bi-trash"></i>
-    <form id="destroy{{ $data->id }}" class="d-none" action="{{ route('product-categories.destroy', $data->id) }}" method="POST">
-        @csrf
-        @method('delete')
-    </form>
-</button>
+
+<form id="destroy{{ $product->id }}" action="{{ route('product-categories.destroy', $product->id) }}" method="POST"
+    class="d-inline" onsubmit="return confirm('Are you sure? It will delete the data permanently!')">
+    @csrf
+    @method('delete')
+    <button type="submit" class="btn btn-danger btn-sm">
+        <i class="bi bi-trash"></i>
+    </button>
+</form>
