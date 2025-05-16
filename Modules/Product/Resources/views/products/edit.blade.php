11697177527654b4ba1f108302d1965d781ad280
KeoChamraeun <keochamraeun54@gmail.com>
2025-05-16 12:00:52 +0700

    dd

--- Modules/Product/Resources/views/products/edit.blade.php
+++ Modules/Product/Resources/views/products/edit.blade.php
@@ -5,8 +5,8 @@
 @section('breadcrumb')
     <ol class="breadcrumb border-0 m-0">
         <li class="breadcrumb-item"><a href="{{ route('home') }}">{{ __('Home') }}</a></li>
-        <li class="breadcrumb-item"><a href="{{ route('products.index') }}">Products</a></li>
-        <li class="breadcrumb-item active">Edit</li>
+        <li class="breadcrumb-item"><a href="{{ route('products.index') }}">{{ __('Products') }}</a></li>
+        <li class="breadcrumb-item active">{{ __('Edit') }}</li>
     </ol>
 @endsection
 
