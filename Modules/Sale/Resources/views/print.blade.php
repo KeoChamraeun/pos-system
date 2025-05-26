f6a950f8b16c944856139f18b8f5c3bd5c73f2f3
KeoChamraeun <keochamraeun54@gmail.com>
2025-05-26 19:47:33 +0700

    dd

--- Modules/Sale/Resources/views/print.blade.php
+++ Modules/Sale/Resources/views/print.blade.php
@@ -1,19 +1,19 @@
-<!doctype html>
-<html lang="kh">
+<!DOCTYPE html>
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ __('Sale Details') }}</title>
-
+    <meta charset="utf-8" />
+    <title>POS Receipt</title>
     <style>
+        /* Use absolute path with file:/// */
         @font-face {
             font-family: 'KhmerOS';
-            src: url('{{ public_path('fonts/KhmerOS_battambang.ttf') }}') format('truetype');
+            src: url('file:///var/www/pos-system/public/fonts/KhmerOS_battambang.ttf') format('truetype');
+            font-weight: normal;
+            font-style: normal;
         }
 
+        /* Apply the font globally */
         * {
             font-family: 'KhmerOS', 'Noto Sans Khmer', sans-serif;
             font-size: 12px;
@@ -86,116 +86,87 @@
 </head>
 
 <body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xs-12">
-                <div style="text-align: center; margin-bottom: 25px;">
-                    <img width="180" src="{{ public_path('images/logo-dark.png') }}" alt="Logo">
-                    <h4 style="margin-bottom: 20px;">
-                        <span>{{ __('Reference') }}::</span> <strong>{{ $sale->reference }}</strong>
-                    </h4>
-                </div>
-
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row mb-4">
-                            <div class="col-xs-4 mb-3 mb-md-0">
-                                <h4 class="mb-2" style="border-bottom: 1px solid #dddddd;padding-bottom: 10px;">
-                                    {{ __('Company Info') }}</h4>
-                                <div><strong>{{ settings()->company_name }}</strong></div>
-                                <div>{{ settings()->company_address }}</div>
-                                <div>{{ __('Email') }}: {{ settings()->company_email }}</div>
-                                <div>{{ __('Phone Number') }}: {{ settings()->company_phone }}</div>
-                            </div>
-
-                            <div class="col-xs-4 mb-3 mb-md-0">
-                                <h4 class="mb-2" style="border-bottom: 1px solid #dddddd;padding-bottom: 10px;">
-                                    {{ __('Customer Info') }}</h4>
-                                <div><strong>{{ $customer->customer_name }}</strong></div>
-                                <div>{{ $customer->address }}</div>
-                                <div>{{ __('Email') }}: {{ $customer->customer_email }}</div>
-                                <div>{{ __('Phone Number') }}: {{ $customer->customer_phone }}</div>
-                            </div>
-
-                            <div class="col-xs-4 mb-3 mb-md-0">
-                                <h4 class="mb-2" style="border-bottom: 1px solid #dddddd;padding-bottom: 10px;">
-                                    {{ __('Invoice Info') }}</h4>
-                                <div>{{ __('Invoice') }}: <strong>INV/{{ $sale->reference }}</strong></div>
-                                <div>{{ __('Date') }}: {{ \Carbon\Carbon::parse($sale->date)->format('d M, Y') }}</div>
-                                <div>{{ __('Status') }}: <strong>{{ $sale->status }}</strong></div>
-                                <div>{{ __('Payment Status') }}: <strong>{{ $sale->payment_status }}</strong></div>
-                            </div>
-                        </div>
-
-                        <div class="table-responsive-sm" style="margin-top: 30px;">
-                            <table class="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th class="align-middle">{{ __('Product') }}</th>
-                                        <th class="align-middle">{{ __('Unit Price') }}</th>
-                                        <th class="align-middle">{{ __('Quantity') }}</th>
-                                        <th class="align-middle">{{ __('Discount') }}</th>
-                                        <th class="align-middle">{{ __('Tax') }}</th>
-                                        <th class="align-middle">{{ __('Sub Total') }}</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($sale->saleDetails as $item)
-                                        <tr>
-                                            <td class="align-middle">
-                                                {{ $item->product_name }} <br>
-                                                <span class="badge badge-success">{{ $item->product_code }}</span>
-                                            </td>
-                                            <td class="align-middle">{{ format_currency($item->unit_price) }}</td>
-                                            <td class="align-middle">{{ $item->quantity }}</td>
-                                            <td class="align-middle">{{ format_currency($item->product_discount_amount) }}
-                                            </td>
-                                            <td class="align-middle">{{ format_currency($item->product_tax_amount) }}</td>
-                                            <td class="align-middle">{{ format_currency($item->sub_total) }}</td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-xs-4 col-xs-offset-8">
-                                <table class="table">
-                                    <tbody>
-                                        <tr>
-                                            <td class="left"><strong>{{ __('Discount') }}
-                                                    ({{ $sale->discount_percentage }}%)</strong></td>
-                                            <td class="right">{{ format_currency($sale->discount_amount) }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="left"><strong>{{ __('Tax') }}
-                                                    ({{ $sale->tax_percentage }}%)</strong></td>
-                                            <td class="right">{{ format_currency($sale->tax_amount) }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="left"><strong>{{ __('Shipping') }}</strong></td>
-                                            <td class="right">{{ format_currency($sale->shipping_amount) }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="left"><strong>{{ __('Grand Total') }}</strong></td>
-                                            <td class="right">
-                                                <strong>{{ format_currency($sale->total_amount) }}</strong></td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-
-                        <div class="row" style="margin-top: 25px;">
-                            <div class="col-xs-12">
-                                <p style="font-style: italic; text-align: center">
-                                    {{ settings()->company_name }} &copy; {{ date('Y') }}.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <div style="max-width:400px;margin:0 auto">
+        <div id="receipt-data">
+            <div class="centered">
+                <h2 style="margin-bottom: 5px">{{ settings()->company_name }}</h2>
+                <p style="font-size: 11px; line-height: 15px; margin-top: 0">
+                    {{ settings()->company_email }}, {{ settings()->company_phone }}<br>
+                    {{ settings()->company_address }}
+                </p>
             </div>
+
+            <p>
+                {{ __('Date') }}: {{ \Carbon\Carbon::parse($sale->date)->format('d M, Y') }}<br>
+                {{ __('Reference') }}: {{ $sale->reference }}<br>
+                {{ __('Name') }}: {{ $sale->customer_name }}
+            </p>
+
+            <table class="table-data">
+                <tbody>
+                    @foreach($sale->saleDetails as $saleDetail)
+                        <tr>
+                            <td colspan="2">
+                                {{ $saleDetail->product->product_name }}
+                                ({{ $saleDetail->quantity }} x {{ format_currency($saleDetail->price) }})
+                            </td>
+                            <td style="text-align:right; vertical-align:bottom">
+                                {{ format_currency($saleDetail->sub_total) }}
+                            </td>
+                        </tr>
+                    @endforeach
+
+                    @if($sale->tax_percentage)
+                        <tr>
+                            <th colspan="2" style="text-align:left">
+                                {{ __('Tax') }} ({{ $sale->tax_percentage }}%)
+                            </th>
+                            <th style="text-align:right">{{ format_currency($sale->tax_amount) }}</th>
+                        </tr>
+                    @endif
+
+                    @if($sale->discount_percentage)
+                        <tr>
+                            <th colspan="2" style="text-align:left">
+                                {{ __('Discount') }} ({{ $sale->discount_percentage }}%)
+                            </th>
+                            <th style="text-align:right">{{ format_currency($sale->discount_amount) }}</th>
+                        </tr>
+                    @endif
+
+                    @if($sale->shipping_amount)
+                        <tr>
+                            <th colspan="2" style="text-align:left">{{ __('Shipping') }}</th>
+                            <th style="text-align:right">{{ format_currency($sale->shipping_amount) }}</th>
+                        </tr>
+                    @endif
+
+                    <tr>
+                        <th colspan="2" style="text-align:left">{{ __('Grand Total') }}</th>
+                        <th style="text-align:right">{{ format_currency($sale->total_amount) }}</th>
+                    </tr>
+                </tbody>
+            </table>
+
+            <table>
+                <tbody>
+                    <tr style="background-color:#ddd;">
+                        <td class="centered" style="padding: 5px;">
+                            {{ __('Paid By') }}: {{ $sale->payment_method }}
+                        </td>
+                        <td class="centered" style="padding: 5px;">
+                            {{ __('Amount') }}: {{ format_currency($sale->paid_amount) }}
+                        </td>
+                    </tr>
+                    <tr style="border-bottom: 0;">
+                        <td class="centered" colspan="3">
+                            <div style="margin-top: 10px;">
+                                {!! \Milon\Barcode\Facades\DNS1DFacade::getBarcodeSVG($sale->reference, 'C128', 1, 25, 'black', false) !!}
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </body>
