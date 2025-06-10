161db2b2869c7caf3c06d686b7d68d3193a328a7
keochamraeun54@gmail.com <keochamraeun54@gmail.com>
2025-06-10 16:02:08 +0700

    dod

--- config/database.php
+++ config/database.php
@@ -65,7 +65,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('DATABASE_URL'), // Optional: used if DATABASE_URL is set
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -77,6 +77,7 @@ return [
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
