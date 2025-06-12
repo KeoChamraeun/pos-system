42cf2eb6ad9d445acb32b750f8d41eaeeed0098f
keochamraeun54@gmail.com <keochamraeun54@gmail.com>
2025-06-12 16:17:48 +0700

    dd

--- resources/views/includes/main-css.blade.php
+++ resources/views/includes/main-css.blade.php
@@ -1,35 +1,40 @@
-<!-- Dropezone CSS -->
+{{-- resources/views/includes/main-css.blade.php --}}
+
+<!-- Dropzone CSS -->
 <link rel="stylesheet" href="{{ asset('css/dropzone.css') }}">
-<!-- CoreUI CSS -->
+
+<!-- CoreUI CSS and your app styles with Vite -->
 @vite('resources/sass/app.scss')
+
+<!-- DataTables CSS -->
 <link href="https://cdn.datatables.net/v/bs4/jszip-3.10.1/dt-1.13.5/b-2.4.1/b-html5-2.4.1/b-print-2.4.1/sl-1.7.0/datatables.min.css" rel="stylesheet">
+
 <!-- Bootstrap Icons -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
+{{-- Additional third-party stylesheets if any --}}
 @yield('third_party_stylesheets')
 
+{{-- Page-specific CSS pushed via @push --}}
 @stack('page_css')
 
+{{-- Livewire Styles --}}
 @livewireStyles
 
 <style>
+    /* DataTables length select */
     div.dataTables_wrapper div.dataTables_length select {
         width: 65px;
         display: inline-block;
     }
-    .select2-container--default .select2-selection--single {
-        background-color: #fff;
-        border: 1px solid #D8DBE0;
-        border-radius: 4px;
-    }
+    /* Select2 styles */
+    .select2-container--default .select2-selection--single,
     .select2-container--default .select2-selection--multiple {
         background-color: #fff;
         border: 1px solid #D8DBE0;
         border-radius: 4px;
     }
-    .select2-container .select2-selection--multiple {
-        height: 35px;
-    }
+    .select2-container .select2-selection--multiple,
     .select2-container .select2-selection--single {
         height: 35px;
     }
