993e80a9d44131966d70085027f57012a8f2e249
keochamraeun54@gmail.com <keochamraeun54@gmail.com>
2025-06-12 17:03:17 +0700

    dd

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
 export default defineConfig({
     plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
+      laravel({
+        input: [
+          'resources/sass/app.scss',
+          'resources/js/app.js',
+          'resources/js/chart-config.js',
+        ],
+        refresh: true,
+      }),
     ],
     server: {
-        host: true,
-        port: 80,
-        strictPort: true,
+      host: true,
+      port: 5173,   // or any port above 1024
+      strictPort: true,
     },
     build: {
-        outDir: 'public/build',
-        assetsDir: '',
-        manifest: true,
+      outDir: 'public/build',
+      assetsDir: '',
+      manifest: true,
     },
-});
+  });
