be6cf7f99d44a620c8f421e205ae431c92d86d66
keochamraeun54@gmail.com <keochamraeun54@gmail.com>
2025-06-13 07:44:57 +0700

    dd

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     ],
     server: {
         host: true,
-        port: 5173,  // Use a non-privileged port like 5173, not 80
+        port: 80,  // Use a non-privileged port like 5173, not 80
         strictPort: true,
     },
     build: {
