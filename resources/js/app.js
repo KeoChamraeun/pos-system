c3ebaf1fd892ab8abe9851a2c70d6d8def2bf83f
keochamraeun54@gmail.com <keochamraeun54@gmail.com>
2025-06-26 10:04:59 +0700

    dd

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,11 @@
 import './bootstrap.js';
 import '@coreui/coreui/dist/js/coreui.bundle.min.js';
+import '../sass/app.scss';
+import 'bootstrap/js/dist/tooltip.js';
+import 'bootstrap/js/dist/popover.js';
+import 'bootstrap/js/dist/dropdown.js';
+import 'bootstrap/js/dist/collapse.js';
+import 'bootstrap/js/dist/modal.js'; 
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,45 +3,36 @@
 
     <head>
         <meta charset="UTF-8">
-        <title>@yield('title') || {{ config('app.name') }}</title>
+        <title>@yield('title', config('app.name'))</title>
         <meta content="Fahim Anzam Dip" name="author">
-        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
         <link rel="icon" href="{{ asset('images/favicon.png') }}">
-        <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Roboto:wght@400;500&display=swap"
-            rel="stylesheet">
-        <link href="{{ asset('css/style.css') }}" rel="stylesheet"> <!-- Laravel correct -->
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
-
-
-
-
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
         <style>
             @font-face {
                 font-family: 'KhmerOS_battambang';
-                src: url('{{ asset('fonts/KhmerOS_battambang.ttf') }}') format('truetype');
+                src: url('{{ asset(' fonts/KhmerOS_battambang.ttf') }}') format('truetype');
                 font-weight: normal;
                 font-style: normal;
             }
-            body {
-                font-family: 'KhmerOS_battambang', sans-serif;
-                font-weight: 400;
-                line-height: 1.6;
-                color: #333;
-            }
+
+            body,
             .c-header,
             .c-subheader,
             .navbar-brand,
             .c-header-nav,
-            .breadcrumb {
-                font-family: 'KhmerOS_battambang', sans-serif;
-                font-weight: 600;
-            }
+            .breadcrumb,
+            .c-main,
+            .btn,
+            .table,
+            .form-control,
+            .custom-select,
+            .c-footer,
             h1,
             h2,
             h3,
@@ -49,32 +40,35 @@
             h5,
             h6 {
                 font-family: 'KhmerOS_battambang', sans-serif;
+            }
+
+            body {
+                font-weight: 400;
+                line-height: 1.6;
+                color: #333;
+            }
+
+            h1,
+            h2,
+            h3,
+            h4,
+            h5,
+            h6 {
                 font-weight: 700;
                 margin-top: 0;
                 margin-bottom: 0.75rem;
             }
-            .c-main {
-                font-family: 'KhmerOS_battambang', sans-serif;
-                font-size: 1rem;
-            }
+
+            .c-header,
+            .c-subheader,
             .btn {
-                font-family: 'KhmerOS_battambang', sans-serif;
                 font-weight: 600;
-            }
-            .table {
-                font-family: 'KhmerOS_battambang', sans-serif;
-            }
-            .form-control,
-            .custom-select {
-                font-family: 'KhmerOS_battambang', sans-serif;
-            }
-            .c-footer {
-                font-family: 'KhmerOS_battambang', sans-serif;
             }
         </style>
 
         @include('includes.main-css')
     </head>
+
 
     <body class="c-app">
         @include('layouts.sidebar')
